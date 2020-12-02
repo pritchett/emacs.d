ad8de8a28dbc8c952fd8a0f4dc300ae5ffe3a88b
Brian Pritchett <brian@brianpritchett.com>
2020-12-02 17:55:12 -0600

    Add scala configuration
    Add metals into configuration. Separate out things into their own modules.

--- init.el
+++ init.el
@@ -1,13 +1,27 @@
+;;; init.el --- Emacs setup entry point
+;;; Commentary:
+;;; Code:
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(flycheck scala-mode which-key monokai-theme magit)))
+ '(line-number-mode nil)
+ '(package-selected-packages
+   '(company-lsp yasnippet lsp-ui lsp-metals lsp-mode sbt-mode use-package flycheck scala-mode which-key monokai-theme magit))
+ '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,4 +42,12 @@
 (which-key-mode 1)
 (global-flycheck-mode)
 
+;; Add Modules and Subdirs to Load Path
+(defvar modules-dir)
+(setq modules-dir (concat user-emacs-directory (convert-standard-filename "modules/")))
+(let ((default-directory modules-dir))
+  (normal-top-level-add-subdirs-to-load-path))
+
+
+(require 'lang-mod)
 ;;; init.el ends here
---
+++ modules/lang/lang-mod.el
@@ -0,0 +1,14 @@
+;;; lang-mod.el --- Programming Language Integration
+;;; Commentary:
+;; This module installs and configures packages for working with various programming languages and tools.
+
+;;; Code:
+;(load "lang-lsp.el")
+(load "scala.el")
+;(load "lang-web.el")
+;(load "lang-rest.el")
+;(load "lang-json.el")
+;(load "lang-hcl.el")
+
+(provide 'lang-mod)
+;;; lang-mod.el ends here
